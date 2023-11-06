89752fb1255f589da5a22d4122e5b61c2e3b27ce
Arso991 <arsogn991@gmail.com>
2023-11-06 18:58:42 +0100

    v4

--- database/index.js
+++ database/index.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv")
 
 dotenv.config();
 
-const MONGODB_CONNECT_URI = "mongodb+srv://arsogn991:kp4nfnzSS3qVHkZd@coursesdb.emifxtz.coursedb.net/?retryWrites=true&w=majority"
+const MONGODB_CONNECT_URI = "mongodb+srv://arsogn991:kp4nfnzSS3qVHkZd@coursesdb.emifxtz.coursedb.net/coursesdb?retryWrites=true&w=majority"
 
 const db = async () =>{
     try {
-        const client = await mongoose.connect(MONGODB_URL);
+        const client = await mongoose.connect(MONGODB_CONNECT_URI);
         console.log(`DB is connected:${client.connection.host}`)
     } catch (error) {
         console.error(error);
